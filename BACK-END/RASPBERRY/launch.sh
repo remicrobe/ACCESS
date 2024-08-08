09db18df0ce280fd02cff5c7d8efc162c0154fb5
remicrobe <64744563+remicrobe@users.noreply.github.com>
2024-08-08 19:16:53 +0200

    some update

--- BACK-END/RASPBERRY/launch.sh
+++ BACK-END/RASPBERRY/launch.sh
@@ -1,1 +1,7 @@
-python3 AccessLink_SCRIPT.py
+#!/bin/bash
+
+# Obtenir le répertoire où se trouve ce script
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+
+# Exécuter le script Python dans ce même répertoire
+python3 "$SCRIPT_DIR/AccessLink_SCRIPT.py"
